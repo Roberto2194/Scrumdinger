32a14d576c86580fe0ea87f4b95c5990161a94a9
Roberto Liccardo <roberto_liccardo@outlook.it>
2023-10-03 18:35:04 +0200

    Added project initial files

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Roberto Liccardo on 03/10/23.
+//
+
+import SwiftUI
+
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
+
---
+++ Scrumdinger/Views/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Roberto Liccardo on 03/10/23.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
