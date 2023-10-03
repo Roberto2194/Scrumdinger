e2d7a776880522a7b51cdc239b113cc6fcdc1108
Roberto Liccardo <roberto_liccardo@outlook.it>
2023-10-03 23:38:02 +0200

    Added DetailView and navigation

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -27,12 +27,16 @@ enum Theme: String {
     
     var accentColor: Color {
         switch self {
-        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
-        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+            case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+            case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
     
     var mainColor: Color {
         Color(rawValue)
+    }
+    
+    var name: String {
+        return rawValue.capitalized
     }
 }
